dee1054f1cd7593ef218a6b9389daf607542c2d6
iffn <iffn1990@gmail.com>
2026-04-01 19:56:47 +0200

    Started adding UI sliders

---
+++ Assets/UI/MaterialFloatSlider.cs
@@ -0,0 +1,83 @@
+﻿
+using TMPro;
+using UdonSharp;
+using UnityEngine;
+using UnityEngine.UI;
+using VRC.SDKBase;
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+public class MaterialFloatSlider : UdonSharpBehaviour
+{
+    [Header("Implementation")]
+    [SerializeField] Material linkedMaterial;
+    [SerializeField] string propertyName;
+    [SerializeField] float min;
+    [SerializeField] float max;
+
+    [Header("Prefab")]
+    [SerializeField] TextMeshProUGUI minText;
+    [SerializeField] TextMeshProUGUI maxText;
+    [SerializeField] Slider slider;
+    [SerializeField] TMP_InputField numericInput;
+
+    [UdonSynced] float syncedValue;
+
+    bool ignoreUpdates = false;
+
+    private void Start()
+    {
+        ignoreUpdates = true;
+        slider.minValue = min;
+        slider.maxValue = max;
+        ignoreUpdates = false;
+        minText.text = min.ToString();
+        maxText.text = max.ToString();
+        
+        float value = linkedMaterial.GetFloat(propertyName);
+        slider.SetValueWithoutNotify(value);
+        numericInput.SetTextWithoutNotify(value.ToString());
+    }
+
+    public override void OnDeserialization()
+    {
+        base.OnDeserialization();
+        slider.SetValueWithoutNotify(syncedValue); // ToDo: Smoothing
+        numericInput.SetTextWithoutNotify(syncedValue.ToString()); // ToDo: Smoothing
+        
+        ApplyValue();
+    }
+
+    public void SliderInputUpdatedLocally()
+    {
+        if(ignoreUpdates)
+            return;
+        syncedValue = slider.value;
+        numericInput.SetTextWithoutNotify(syncedValue.ToString());
+        SyncAndApplyValue();
+    }
+
+    public void NumericInputUpdatedLocally()
+    {
+        if (ignoreUpdates)
+            return;
+        syncedValue = Mathf.Clamp(int.Parse(numericInput.text), min, max);
+        numericInput.SetTextWithoutNotify(syncedValue.ToString());
+        slider.SetValueWithoutNotify(syncedValue);
+        SyncAndApplyValue();
+    }
+
+    void SyncAndApplyValue()
+    {
+        if (Networking.IsOwner(gameObject))
+            Networking.SetOwner(Networking.LocalPlayer, gameObject);
+
+        RequestSerialization(); // ToDo: Slow down rate
+
+        ApplyValue();
+    }
+
+    void ApplyValue()
+    {
+        linkedMaterial.SetFloat(propertyName, syncedValue);
+    }
+}
---
+++ Assets/UI/MaterialRGBSlider.cs
@@ -0,0 +1,92 @@
+﻿
+using TMPro;
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using UnityEngine.UI;
+using VRC.Udon;
+
+public class MaterialRGBSlider : UdonSharpBehaviour
+{
+    [Header("Implementation")]
+    [SerializeField] Material linkedMaterial;
+    [SerializeField] string propertyName;
+
+    [Header("Prefab")]
+    [SerializeField] Slider sliderRed;
+    [SerializeField] Slider sliderGreen;
+    [SerializeField] Slider sliderBlue;
+    [SerializeField] TMP_InputField numericInputRed;
+    [SerializeField] TMP_InputField numericInputGreen;
+    [SerializeField] TMP_InputField numericInputBlue;
+
+    [UdonSynced] Color syncedColor;
+
+    bool ignoreUpdates = false;
+
+    private void Start()
+    {
+        Color value = linkedMaterial.GetColor(propertyName);
+    }
+
+    public override void OnDeserialization()
+    {
+        base.OnDeserialization();
+        WriteToSliders(syncedColor); // ToDo: Smoothing
+        WriteToNumericInputs(syncedColor); // ToDo: Smoothing
+
+        ApplyValue();
+    }
+
+    public void SliderInputUpdatedLocally()
+    {
+        syncedColor.r = sliderRed.value;
+        syncedColor.g = sliderGreen.value;
+        syncedColor.b = sliderBlue.value;
+
+        WriteToNumericInputs(syncedColor);
+
+        SyncAndApplyValue();
+    }
+
+    public void NumericInputUpdatedLocally()
+    {
+        syncedColor.r = Mathf.Clamp(int.Parse(numericInputRed.text), 0f, 1f);
+        syncedColor.g = Mathf.Clamp(int.Parse(numericInputGreen.text), 0f, 1f);
+        syncedColor.b = Mathf.Clamp(int.Parse(numericInputBlue.text), 0f, 1f);
+
+        WriteToNumericInputs(syncedColor); // Do because of clamp
+        WriteToSliders(syncedColor);
+
+        SyncAndApplyValue();
+    }
+
+    void WriteToSliders(Color color)
+    {
+        sliderRed.value = color.r;
+        sliderGreen.value = color.g;
+        sliderBlue.value = color.b;
+    }
+
+    void WriteToNumericInputs(Color color)
+    {
+        numericInputRed.SetTextWithoutNotify(color.r.ToString());
+        numericInputGreen.SetTextWithoutNotify(color.g.ToString());
+        numericInputBlue.SetTextWithoutNotify(color.b.ToString());
+    }
+
+    void SyncAndApplyValue()
+    {
+        if (Networking.IsOwner(gameObject))
+            Networking.SetOwner(Networking.LocalPlayer, gameObject);
+
+        RequestSerialization(); // ToDo: Slow down rate
+
+        ApplyValue();
+    }
+
+    void ApplyValue()
+    {
+        linkedMaterial.SetColor(propertyName, syncedColor);
+    }
+}
