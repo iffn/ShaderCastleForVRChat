46b80709e7ffe1f23bd12db019679d0075317b86
iffn <iffn1990@gmail.com>
2026-06-24 15:27:39 +0200

    Created utility scripts

---
+++ Assets/UI/UIInterface.cs
@@ -0,0 +1,109 @@
+#if UNITY_EDITOR
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using TMPro;
+using System.Linq;
+using UnityEditor.SceneManagement;
+
+[CustomEditor(typeof(UIInterface))]
+public class UIInterfaceEditor : Editor
+{
+    UIInterface linkedDisplayOrganizer => (UIInterface)target;
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        if (GUILayout.Button("Clean up"))
+        {
+            linkedDisplayOrganizer.CleanUp();
+        }
+
+        if (GUILayout.Button("Clean up all rect transforms"))
+        {
+            PrefabStage currentStage = PrefabStageUtility.GetCurrentPrefabStage();
+
+            RectTransform[] allRects;
+
+            if (currentStage == null)
+            {
+                allRects = Object.FindObjectsByType<RectTransform>(FindObjectsSortMode.None);
+            }
+            else
+            {
+                GameObject prefabRoot = currentStage.prefabContentsRoot;
+                allRects = prefabRoot.GetComponentsInChildren<RectTransform>(true);
+            }
+
+            int reset = 0;
+
+            foreach (RectTransform rect in allRects)
+            {
+                if (rect.gameObject == ((MonoBehaviour)target).gameObject)
+                    continue;
+
+                UnityEditor.PropertyModification[] modifications = PrefabUtility.GetPropertyModifications(rect);
+        
+                // If the modifications array is not null and has items, it has overrides
+                if (modifications != null && modifications.Length > 0)
+                {
+                    PrefabUtility.RevertObjectOverride(rect, InteractionMode.AutomatedAction);
+                    reset++;
+                }
+                
+            }
+
+            Debug.Log($"Reset: {reset}");
+        }
+    }
+}
+
+public class UIInterface : MonoBehaviour
+{
+    [SerializeField] TMP_Text tile;
+    [SerializeField] TMP_Text description;
+
+    public string Title
+    {
+        get
+        {
+            return tile.text;
+        }
+        set
+        {
+            tile.text = value;
+        }
+    }
+
+    public string Description
+    {
+        get
+        {
+            return description.text;
+        }
+        set
+        {
+            description.text = value;
+        }
+    }
+
+    public void CleanUp()
+    {
+        // No spaces in title
+        string originalString = tile.text;
+        string cleanString = originalString.Replace("\r", "").Replace("\n", "");
+        tile.text = cleanString;
+
+        // Clean up rect transform
+        RectTransform[] rectTransforms = transform.GetComponentsInChildren<RectTransform>(true);
+        foreach (RectTransform rect in rectTransforms)
+        {
+            PrefabUtility.RevertObjectOverride(rect, InteractionMode.AutomatedAction);
+        }
+    }
+
+    
+}
+#endif
---
+++ Prefabs/ShaderDisplay.cs
@@ -0,0 +1,103 @@
+#if UNITY_EDITOR
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using TMPro;
+using VRC.SDK3.Editor;
+using UnityEditor.SceneManagement;
+
+[CustomEditor(typeof(ShaderDisplay))]
+public class ShaderDisplayEditor : Editor
+{
+    ShaderDisplay linkedDisplay => (ShaderDisplay)target;
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        if (GUILayout.Button("Get"))
+        {
+            linkedDisplay.GetData();
+        }
+
+        if (GUILayout.Button("Set"))
+        {
+            linkedDisplay.SetData();
+        }
+
+        if (GUILayout.Button("Set all"))
+        {
+            PrefabStage currentStage = PrefabStageUtility.GetCurrentPrefabStage();
+
+            ShaderDisplay[] allShaderDisplays;
+
+            if (currentStage == null)
+            {
+                allShaderDisplays = Object.FindObjectsByType<ShaderDisplay>(FindObjectsSortMode.None);
+            }
+            else
+            {
+                GameObject prefabRoot = currentStage.prefabContentsRoot;
+                allShaderDisplays = prefabRoot.GetComponentsInChildren<ShaderDisplay>(true);
+            }
+
+            foreach (ShaderDisplay display in allShaderDisplays)
+            {
+                display.SetData();
+            }
+        }
+
+        if (GUILayout.Button("Get all"))
+        {
+            PrefabStage currentStage = PrefabStageUtility.GetCurrentPrefabStage();
+
+            ShaderDisplay[] allShaderDisplays;
+
+            if (currentStage == null)
+            {
+                allShaderDisplays = Object.FindObjectsByType<ShaderDisplay>(FindObjectsSortMode.None);
+            }
+            else
+            {
+                GameObject prefabRoot = currentStage.prefabContentsRoot;
+                allShaderDisplays = prefabRoot.GetComponentsInChildren<ShaderDisplay>(true);
+            }
+
+            foreach (ShaderDisplay display in allShaderDisplays)
+            {
+                display.GetData();
+            }
+        }
+    }
+}
+
+public class ShaderDisplay : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] TMP_Text titleElement;
+    [SerializeField] UIInterface linkedInterface;
+    [SerializeField] MeshRenderer linkedMeshRenderer;
+
+    [Header("Values")]
+    [SerializeField] string title;
+    [SerializeField] [TextArea(3, 10)] string description;
+    [SerializeField] Material linkedMaterial;
+
+    public void GetData()
+    {
+        title = titleElement.text;
+        title = title.Replace("\r", "").Replace("\n", " ");
+        description = linkedInterface.Description;
+        linkedMaterial = linkedMeshRenderer.sharedMaterial;
+    }
+
+    public void SetData()
+    {
+        titleElement.text = title;
+        linkedInterface.Description = description;
+        linkedMeshRenderer.sharedMaterial = linkedMaterial;
+    }
+
+}
+#endif
